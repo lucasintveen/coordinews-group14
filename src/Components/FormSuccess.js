726d71cd302f5872853eafcbb4d69773e550a7df
Lucas Intveen <70695669+lucasintveen@users.noreply.github.com>
2021-12-22 12:46:22 +0100

    Update FormSuccess.js

--- src/Components/FormSuccess.js
+++ src/Components/FormSuccess.js
@@ -2,10 +2,16 @@ import React from "react";
 import Start from "../Images/Start.png";
 import "../CSS/Form.css";
 
-const FormSuccess = () => {
+const FormSuccess = (props) => {
   return (
     <div className="form-content-right">
-      <h1 className="form-success">We have received your request!</h1>
+      {!props.formTypeArticle ? (
+        <h1 className="form-success">
+          You have successfully deleted the article.
+        </h1>
+      ) : (
+        <h1 className="form-success">TEST 123</h1>
+      )}
     </div>
   );
 };
